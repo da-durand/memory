4c92a5030577190f26dbb2618d2363f1fa199f6b
dadurand <ha.halo@live.fr>
2019-09-30 12:07:36 +0200

    phase 2

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ document.body.appendChild(mainElement);
 var ulElement = document.createElement("ul");
 mainElement.appendChild(ulElement);
 
-var nb = 9;
+var nb = 12;
+var nbList = [];
 
-for (let i = 1; i <= nb; i++){
+var card1 = "";
+var card2 = "";
+
+for (let i = 0; i < nb/2; i++){
+    nbList.push(i);
+    nbList.push(i);
+}
+
+for (let i = 0; i < nbList.length; i++){
+    var random_index = Math.floor(Math.random() * (i + 1));
+    var temp = nbList[i];
+    nbList[i] = nbList[random_index];
+    nbList[random_index] = temp;
+}
+
+for (let i = 0; i < nb; i++){
     var liElement = document.createElement("li");
     ulElement.appendChild(liElement);
     liElement.className = "card";
-    liElement.innerHTML = i;
-
+    liElement.innerHTML = nbList[i];
     liElement.addEventListener("click", function(){
         if (this.classList.contains("card")){
             this.classList.remove("card");
